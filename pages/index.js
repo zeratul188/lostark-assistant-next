e41642d1250b1ec852d6929f4a6d0a3ead04b143
Seongho Park <bluetosh7@gmail.com>
2022-01-22 17:09:37 +0900

    feat: add emotionjs

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import {css} from '@emotion/react'
 
 export default function Home() {
   return (
@@ -12,7 +13,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 css={css`color: red;`} className={styles.title} >
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
